34c1df3eeb4737bae199e59bf456097ddb2151f5
santibritos <britos.santiago022@gmail.com>
2023-06-15 23:55:11 -0300

    login css

--- resources/views/index/login.blade.php
+++ resources/views/index/login.blade.php
@@ -1,28 +1,50 @@
-@extends('index.plantilla')
-@section('content1')
-<div class="container w-25 border p-4 mt-4">
-<form action="/menu" method="get" onsubmit="return validateForm()">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Direccion Email</label>
-    <input type="email" class="form-control" id="email1" name="email">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-    <input type="password" class="form-control" id="password1" name="password">
-  </div>
-  <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-</form>
-</div>
 
-<script>
-function validateForm() {
-  var email = document.getElementById('email1').value;
-  var password = document.getElementById('password1').value;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>SACC</title>
+      
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href= "{{asset('app.css')}} ">
+      <link rel="stylesheet" href= "{{asset('login.css')}} ">
+  </head>
+      <section class="side">
+        <div class="titulo"><h1 class="h1-title">SACC</h1></div>
+      </section>
+      <section class="main">
+    <body>
+        <div class="login-container">
+        <form action="/menu" method="get" onsubmit="return validateForm()">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Correo electrónico</label>
+            <input type="email" class="form-control" id="email1" name="email">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="password1" name="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+        </form>
+        </div>
 
-  if (email.trim() === '' || password.trim() === '') {
-    alert('Por favor, complete todos los campos.');
-    return false;
-  }
-}
-</script>
-@endsection
+        <script>
+        function validateForm() {
+          var email = document.getElementById('email1').value;
+          var password = document.getElementById('password1').value;
+
+          if (email.trim() === '' || password.trim() === '') {
+            alert('Por favor, complete todos los campos.');
+            return false;
+          }
+        }
+        </script>
+
+      </section> 
+    </body>
+</html>
+
