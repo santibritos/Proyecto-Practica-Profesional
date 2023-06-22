78bc87d5ea5a2fb7f126079552db7dce79d57467
santibritos <britos.santiago022@gmail.com>
2023-06-22 18:19:50 -0300

    cambio de rutas
    carpeta "tablasMaestras"

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('crearUsuario', function () {
     return view('index.crearUsuario');
 });
 
-Route::get('menu', function () {
+Route::get('/menu', function () {
     return view('index.menu');
 });
 
@@ -33,14 +33,15 @@ Route::get('navbar', function () {
     return view('index.navbar');
 });
 
-Route::get('plantilla', function () {
-    return view('index.plantilla');
+Route::get('/cuenta', function () {
+    return view('/tablasMaestras.cuenta');
 });
 
-Route::get('cuenta', function () {
-    return view('index.cuenta');
+Route::get('/subcuenta', function () {
+    return view('/tablasMaestras.subcuenta');
 });
 
-Route::get('subcuenta', function () {
-    return view('index.subcuenta');
+Route::get('/carreras', function(){
+    return view('tablasMaestras.carreras');
 });
+
